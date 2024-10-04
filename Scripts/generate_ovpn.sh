6261a11fd4eb7dec88a4e211f4ed9dd728a73663
SysItWorking <sysitworking@gmail.com>
2024-10-04 12:38:16 +0300

    Update generate_ovpn.sh

--- Scripts/generate_ovpn.sh
+++ Scripts/generate_ovpn.sh
@@ -58,7 +58,7 @@ if [ -f "$CLIENT_CERT" ] || [ -f "$CLIENT_KEY" ]; then
 
         # Generate a new certificate
         cd /root/work/openvpn-ca || exit
-        ./easyrsa build-client-full "$CLIENT_NAME" nopass
+        ./easyrsa --batch build-client-full "$CLIENT_NAME" nopass
 
         echo -e "\e[32mBackup of removed files you can find here: $BACKUP_DIR\e[0m"
     else
@@ -69,7 +69,7 @@ else
     # Generate a new certificate if it doesn't exist
     echo "Generating a new certificate for the client $CLIENT_NAME..."
     cd /root/work/openvpn-ca || exit
-    ./easyrsa build-client-full "$CLIENT_NAME" nopass
+    ./easyrsa --batch build-client-full "$CLIENT_NAME" nopass
 fi
 
 # Check if the certificate has been created successfully
